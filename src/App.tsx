33ca366cebdd5bd97f0cbc1e8930bb63150c9f2d
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2025-09-24 20:29:42 -0300

    fix scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { PropsWithChildren, useLayoutEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Divider } from './components/divider';
 import { Footer } from './components/footer';
 import { Navbar } from './components/navbar';
@@ -9,17 +10,30 @@ import Loveg from './pages/cases-projects/loveg';
 import Store from './pages/cases-projects/store-api';
 import Home from './pages/home';
 
+const Wrapper = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
+  return children;
+};
+
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path={CASES.STORE} element={<Store />} />
-        <Route path={CASES.APP_BROKER} element={<AppBroker />} />
-        <Route path={CASES.FREIGHT_LOGIN} element={<FreightLogin />} />
-        <Route path={PROJECTS.LOVEG} element={<Loveg />} />
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={CASES.STORE} element={<Store />} />
+          <Route path={CASES.APP_BROKER} element={<AppBroker />} />
+          <Route path={CASES.FREIGHT_LOGIN} element={<FreightLogin />} />
+          <Route path={PROJECTS.LOVEG} element={<Loveg />} />
+        </Routes>
+      </Wrapper>
       <Divider />
       <Footer />
     </>
